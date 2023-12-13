8aca9856d8285503c3731cf7f9e06f936475f094
Cheng Peng <153715087+chengzju@users.noreply.github.com>
2023-12-13 17:31:43 +0800

    Add files via upload

---
+++ scripts/fewasp_multi.sh
@@ -0,0 +1,40 @@
+
+python main.py \
+--freeze_bert \
+--early_stop \
+--shuffle \
+--dataset_idx 2 \
+--config_idx 0 \
+--gpuid 0 \
+--seed 1 \
+--id 1 \
+
+python main.py \
+--freeze_bert \
+--early_stop \
+--shuffle \
+--dataset_idx 2 \
+--config_idx 1 \
+--gpuid 0 \
+--seed 1 \
+--id 1 \
+
+python main.py \
+--freeze_bert \
+--early_stop \
+--shuffle \
+--dataset_idx 2 \
+--config_idx 2 \
+--gpuid 0 \
+--seed 1 \
+--id 1 \
+
+python main.py \
+--freeze_bert \
+--early_stop \
+--shuffle \
+--dataset_idx 2 \
+--config_idx 3 \
+--gpuid 0 \
+--seed 1 \
+--id 1 \
---
+++ scripts/fewasp_single.sh
@@ -0,0 +1,41 @@
+
+python main.py \
+--freeze_bert \
+--early_stop \
+--shuffle \
+--dataset_idx 1 \
+--config_idx 0 \
+--gpuid 0 \
+--seed 1 \
+--id 1 \
+
+python main.py \
+--freeze_bert \
+--early_stop \
+--shuffle \
+--dataset_idx 1 \
+--config_idx 1 \
+--gpuid 0 \
+--seed 1 \
+--id 1 \
+
+python main.py \
+--freeze_bert \
+--early_stop \
+--shuffle \
+--dataset_idx 1 \
+--config_idx 2 \
+--gpuid 0 \
+--seed 1 \
+--id 1 \
+
+python main.py \
+--freeze_bert \
+--early_stop \
+--shuffle \
+--dataset_idx 1 \
+--config_idx 3 \
+--gpuid 0 \
+--seed 1 \
+--id 1 \
+
